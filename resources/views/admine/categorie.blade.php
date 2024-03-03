4fd479dee3dab5316ccb64a7f0018c480f6d5325
Douaa <douaachemnae@gmail.com>
2024-03-03 13:56:26 +0100

    start doashbord Admin

---
+++ resources/views/admine/categorie.blade.php
@@ -0,0 +1,143 @@
+<x-doashboardAdmin></x-doashboardAdmin>
+<div class="container mx-auto mt-5 ">
+
+    <h2 class="text-xl font-bold mb-4 mx-5">Liste des Catégories</h2>
+
+    <div class="flex justify-end mb-2 ">
+        <button id="openAddModal" class="bg-green-500 hover:bg-green-700 text-white mx-2 font-bold py-2 px-4 rounded inline-flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
+            </svg>
+            Ajouter
+        </button>
+    </div>
+
+    <div class="container mx-auto mt-5 flex justify-center  ">
+        <table class="table-auto  w-auto mt-3 ">
+        <thead>
+            <tr>
+                <th class="border px-4 py-2">Nom</th>
+                <th class="border px-4 py-2">Opération</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($categorie as $categories)
+            <tr>
+                <td class="border px-4 py-2">{{ $categories->nom }}</td>
+                <td class="border px-4 py-2 flex justify-around">
+                    <button class="btn-edit bg-blue-500 hover:bg-blue-700 text-white mx-4 font-bold py-1 px-2 rounded flex items-center" data-id="" data-nom="">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"/>
+                        </svg>
+                        Modifier
+                    </button>
+                    
+                    <a onclick="return confirm('Are you sure to delete this?')" href="">
+                        <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
+                            </svg>
+                            Supprimer
+                        </button>
+                    </a>
+                    
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+       
+            
+            
+    </table>
+
+</div>
+
+
+<!-- Pop-up Ajouter Spécialité -->
+<div id="addModal" class="fixed inset-0 bg-gray-900 bg-opacity-50 hidden items-center justify-center">
+    <div class="bg-white p-4 rounded-lg max-w-sm mx-auto">
+        <form action="" method="POST">
+            @csrf
+            <div class="mb-4">
+                <label for="specialiteNom" class="block text-gray-700 text-sm font-bold mb-2">Nom de Catégorie:</label>
+                <input type="text" name="nom" id="specialiteNom" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
+            </div>
+            <div class="flex items-center justify-between">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded " type="submit">
+                    Ajouter
+                </button>
+                <button type="button" id="closeAddModal" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+                    Fermer
+                </button>
+            </div>
+        </form>        
+    </div>
+</div>
+
+
+<!-- Pop-up de Modification -->
+<div id="modalEdit" class="hidden fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full">
+    <div class="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
+        <form id="editForm" action="" method="POST">
+            @csrf
+            <input type="hidden" name="_method" value="PUT">
+            @method('PUT')
+            <input type="hidden" id="specialiteId" name="id">
+            <div class="mb-4">
+                <label for="editNom" class="block text-gray-700 text-sm font-bold mb-2">Nom de Catégorie</label>
+                <input type="text" id="editNom" name="nom" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
+            </div>
+            <div class="flex items-center justify-between">
+                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Modifier
+                </button>
+                <button type="button" id="closeEditModal" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                    Fermer
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
+
+
+
+<script>
+    document.getElementById('openAddModal').addEventListener('click', function() {
+        document.getElementById('addModal').classList.remove('hidden');
+    });
+
+    document.getElementById('closeAddModal').addEventListener('click', function() {
+        document.getElementById('addModal').classList.add('hidden');
+    });
+    </script>
+
+
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const editButtons = document.querySelectorAll('.btn-edit');
+        const modalEdit = document.getElementById('modalEdit');
+        const closeEditModal = document.getElementById('closeEditModal');
+    
+        editButtons.forEach(button => {
+            button.addEventListener('click', function() {
+                const id = button.getAttribute('data-id');
+                const nom = button.getAttribute('data-nom'); // Assurez-vous que cette ligne utilise `nom` pour correspondre avec l'attribut.
+    
+                const form = document.getElementById('editForm');
+                form.action = ``; // Met à jour l'action avec l'ID correct
+                document.getElementById('editNom').value = nom; // Assurez-vous que cette ligne utilise la même variable `nom`.
+    
+                modalEdit.classList.remove('hidden');
+            });
+        });
+    
+        closeEditModal.addEventListener('click', function() {
+            modalEdit.classList.add('hidden');
+        });
+    });
+    </script>
+    
+    
+    
+</body>
---
+++ resources/views/components/doashboardAdmin.blade.php
@@ -0,0 +1,115 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="../public/image/licon.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="../public/css/style.css">
+    <title>Evento</title>
+</head>
+<body class="bg-gray-100 dark:bg-gray-800">
+    <main class="relative min-h-screen flex flex-col">
+        <div class="flex flex-1 overflow-auto">
+    <div class="flex items-start justify-between">
+        <aside class="hidden lg:block shadow-lg w-80 h-full ">
+            <div class="h-full bg-white dark:bg-gray-700 ">
+                <div class="flex items-center justify-start pt-6 ml-8">
+                    <p class="text-xl font-bold dark:text-white">
+                        Plannifer
+                    </p>
+                </div>
+                <nav class="mt-6">
+                    <div>
+                        <a class="flex items-center justify-start w-full p-2 pl-6 my-2 text-gray-800 transition-colors duration-200 border-l-4 border-red-500 dark:text-white" href="#">
+                            <span class="text-left">
+                                <svg width="20" height="20" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M1472 992v480q0 26-19 45t-45 19h-384v-384h-256v384h-384q-26 0-45-19t-19-45v-480q0-1 .5-3t.5-3l575-474 575 474q1 2 1 6zm223-69l-62 74q-8 9-21 11h-3q-13 0-21-7l-692-577-692 577q-12 8-24 7-13-2-21-11l-62-74q-8-10-7-23.5t11-21.5l719-599q32-26 76-26t76 26l244 204v-195q0-14 9-23t23-9h192q14 0 23 9t9 23v408l219 182q10 8 11 21.5t-7 23.5z">
+                                    </path>
+                                </svg>
+                            </span>
+                            <span class="mx-2 text-sm font-normal">
+                                Home
+                            </span>
+                        </a>
+                        <a class="flex items-center justify-start w-full p-2 pl-6 my-2 text-gray-400 transition-colors duration-200 border-l-4 border-transparent hover:text-gray-800" href="#">
+                            <span class="text-left">
+                                <svg width="20" height="20" fill="currentColor" viewBox="0 0 2048 1792" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M1070 1178l306-564h-654l-306 564h654zm722-282q0 182-71 348t-191 286-286 191-348 71-348-71-286-191-191-286-71-348 71-348 191-286 286-191 348-71 348 71 286 191 191 286 71 348z">
+                                    </path>
+                                </svg>
+                            </span>
+                            <span class="mx-2 text-sm font-normal">
+                                Evenment
+                                <span class="w-4 h-2 p-1 ml-4 text-xs text-gray-400 bg-gray-200 rounded-lg">
+                                    0
+                                </span>
+                            </span>
+                        </a>
+                        <a class="flex items-center justify-start w-full p-2 pl-6 my-2 text-gray-400 transition-colors duration-200 border-l-4 border-transparent hover:text-gray-800" href="#">
+                            <span class="text-left">
+                                <svg width="20" height="20" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M1728 608v704q0 92-66 158t-158 66h-1216q-92 0-158-66t-66-158v-960q0-92 66-158t158-66h320q92 0 158 66t66 158v32h672q92 0 158 66t66 158z">
+                                    </path>
+                                </svg>
+                            </span>
+                            <span class="mx-4 text-sm font-normal">
+                               Gestion de catégories
+                            </span>
+                            
+                        </a>
+                        <a class="flex items-center justify-start w-full p-2 pl-6 my-2 text-gray-400 transition-colors duration-200 border-l-4 border-transparent hover:text-gray-800" href="#">
+                            <span class="text-left">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                                  </svg>
+                                  
+                            </span>
+                            <span class="mx-4 text-sm font-normal">
+                               utilisaters
+                            </span>
+                        </a>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <div class="flex flex-col w-full md:space-y-4">
+            <header class="z-40 flex items-center justify-between w-full h-16">
+                <div class="block ml-6 lg:hidden">
+                    <button class="flex items-center p-2 text-gray-500 bg-white rounded-full shadow text-md">
+                        <svg width="20" height="20" class="text-gray-400" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1664 1344v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45z">
+                            </path>
+                        </svg>
+                    </button>
+                </div>
+                <div class="relative z-20 flex flex-col justify-end h-full px-3 md:w-full">
+                    <div class="relative flex items-center justify-end w-full p-1 space-x-4">
+                       
+                        <button class="flex items-center p-2 text-gray-400 bg-white rounded-full shadow hover:text-gray-700 text-md">
+                            <svg width="20" height="20" class="text-gray-400" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M912 1696q0-16-16-16-59 0-101.5-42.5t-42.5-101.5q0-16-16-16t-16 16q0 73 51.5 124.5t124.5 51.5q16 0 16-16zm816-288q0 52-38 90t-90 38h-448q0 106-75 181t-181 75-181-75-75-181h-448q-52 0-90-38t-38-90q50-42 91-88t85-119.5 74.5-158.5 50-206 19.5-260q0-152 117-282.5t307-158.5q-8-19-8-39 0-40 28-68t68-28 68 28 28 68q0 20-8 39 190 28 307 158.5t117 282.5q0 139 19.5 260t50 206 74.5 158.5 85 119.5 91 88z">
+                                </path>
+                            </svg>
+                        </button>
+                        <span class="w-1 h-8 bg-gray-200 rounded-lg">
+                        </span>
+                        {{-- image --}}
+                        <a href="#" class="relative block">
+                          
+                        </a>
+                        <button class="flex items-center text-gray-500 dark:text-white text-md">
+                            Charlie R
+                            <svg width="20" height="20" class="ml-2 text-gray-400" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1408 704q0 26-19 45l-448 448q-19 19-45 19t-45-19l-448-448q-19-19-19-45t19-45 45-19h896q26 0 45 19t19 45z">
+                                </path>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </header>
+       
